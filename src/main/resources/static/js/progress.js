21c771eabbe433de93f9b1674790f3f38add0c22
Adrian Jankowski <jankowski.adrian13@gmail.com>
2024-02-12 22:53:13 +0100

    Added new buttons for chart on progress page.

--- src/main/resources/static/js/progress.js
+++ src/main/resources/static/js/progress.js
@@ -1,113 +1,127 @@
-    var chart = null;
+var chart = null;
+var chosenMeasurementSize = 1;
+var chosenMeasurementType = 'weight';
 
-    function showMeasurement(measurementId, listId) {
-        $.get('/progress/measurement', { id: measurementId})
-          .done(function(data) {
-            const elements = document.querySelectorAll('.chosen-result');
-            elements.forEach(element => {
-                element.classList.remove('chosen-result');
-            });
+function showMeasurement(measurementId, listId) {
+    $.get('/progress/measurement', { id: measurementId})
+      .done(function(data) {
+        const elements = document.querySelectorAll('.chosen-result');
+        elements.forEach(element => {
+            element.classList.remove('chosen-result');
+        });
 
-            document.getElementById("left-biceps-value").textContent=data.leftBiceps;
-            document.getElementById("right-biceps-value").textContent=data.rightBiceps;
-            document.getElementById("chest-value").textContent=data.chest;
-            document.getElementById("waist-value").textContent=data.waist;
-            document.getElementById("left-thigh-value").textContent=data.leftThigh;
-            document.getElementById("right-thigh-value").textContent=data.rightThigh;
-            document.getElementById("weight-value").textContent=data.weight;
-            document.getElementById("result-id-" + listId).className += " chosen-result";
-          })
-          .fail(function(error) {
-            console.error('Błąd:', error);
-          });
-    }
+        document.getElementById("left-biceps-value").textContent=data.leftBiceps;
+        document.getElementById("right-biceps-value").textContent=data.rightBiceps;
+        document.getElementById("chest-value").textContent=data.chest;
+        document.getElementById("waist-value").textContent=data.waist;
+        document.getElementById("left-thigh-value").textContent=data.leftThigh;
+        document.getElementById("right-thigh-value").textContent=data.rightThigh;
+        document.getElementById("weight-value").textContent=data.weight;
+        document.getElementById("result-id-" + listId).className += " chosen-result";
+      })
+      .fail(function(error) {
+        console.error('Błąd:', error);
+      });
+}
 
-    function clickAndDraw(size, orderBy, sortOrder, whatToDisplay) {
-        $.get('/progress/measurements', { page: 0, size: size, orderBy: orderBy, sortOrder: sortOrder })
-          .done(function(data) {
-            var mappedData = data.map(function(item) {
-                let yValue;
-                switch(whatToDisplay) {
-                    case 'leftBiceps':
-                        yValue = item.leftBiceps;
-                        break;
-                    case 'rightBiceps':
-                        yValue = item.rightBiceps;
-                        break;
-                    case 'chest':
-                        yValue = item.chest;
-                        break;
-                    case 'waist':
-                        yValue = item.waist;
-                        break;
-                    case 'leftThigh':
-                        yValue = item.leftThigh;
-                        break;
-                    case 'rightThigh':
-                        yValue = item.rightThigh;
-                        break;
-                    case 'weight':
-                    default:
-                        yValue = item.weight;
-                        break;
-                }
-                return {x: item.createdAt, y:yValue}
-            });
-            drawChart(mappedData);
-          })
-          .fail(function(error) {
-            console.error('Błąd:', error);
-          });
-    }
+function clickAndDraw(obj, size, orderBy, sortOrder, whatToDisplay) {
+    const elements = document.querySelectorAll('.measurement-stat');
+    elements.forEach(element => {
+        element.classList.remove('underlined');
+    });
+    obj.classList.add('underlined');
 
-    function drawChart(map) {
-        let mapAsMap = new Map(Object.entries(map));
-        let keys = Array.from(mapAsMap.keys());
-        let values = Array.from(mapAsMap.values());
-        let maxValue = Math.max(...values);
+    if(size === 0)
+        size = chosenMeasurementSize;
+    if(whatToDisplay.length === 0)
+        whatToDisplay = chosenMeasurementType;
+    chosenMeasurementSize = size;
+    chosenMeasurementType = whatToDisplay;
 
-        if(chart != null)
-            chart.destroy();
+    $.get('/progress/measurements', { page: 0, size: size, orderBy: orderBy, sortOrder: sortOrder })
+      .done(function(data) {
+        var mappedData = data.map(function(item) {
+            let yValue;
+            chosenMeasurementSize = size;
+            switch(whatToDisplay) {
+                case 'leftBiceps':
+                    yValue = item.leftBiceps;
+                    break;
+                case 'rightBiceps':
+                    yValue = item.rightBiceps;
+                    break;
+                case 'chest':
+                    yValue = item.chest;
+                    break;
+                case 'waist':
+                    yValue = item.waist;
+                    break;
+                case 'leftThigh':
+                    yValue = item.leftThigh;
+                    break;
+                case 'rightThigh':
+                    yValue = item.rightThigh;
+                    break;
+                case 'weight':
+                default:
+                    yValue = item.weight;
+                    break;
+            }
+            return {x: item.createdAt, y:yValue}
+        });
+        drawChart(mappedData);
+      })
+      .fail(function(error) {
+        console.error('Błąd:', error);
+      });
+}
 
-         chart = new Chart("myChart", {
-            type: "line",
-            data: {
-                datasets: [{
-                    borderColor: "rgba(0,0,255,0.1)",
-                    data: map
-                }]
-            },
-            options: {
-                animation: false,
-                plugins: {
-                    legend: {
-                        display: false
-                    },
+function drawChart(map) {
+    let mapAsMap = new Map(Object.entries(map));
+    let keys = Array.from(mapAsMap.keys());
+    let values = Array.from(mapAsMap.values());
+    let maxValue = Math.max(...values);
+
+    if(chart != null)
+        chart.destroy();
+
+     chart = new Chart("myChart", {
+        type: "line",
+        data: {
+            datasets: [{
+                borderColor: "rgba(0,0,255,0.1)",
+                data: map
+            }]
+        },
+        options: {
+            animation: false,
+            plugins: {
+                legend: {
+                    display: false
                 },
-                scales: {
-                    x: {
-                        title: {
-                            text: 'Data pomiaru',
-                            display: true,
-                            font: {
-                                size: 20,
-                                style: 'bold'
-                            }
+            },
+            scales: {
+                x: {
+                    title: {
+                        text: 'Data pomiaru',
+                        display: false,
+                        font: {
+                            size: 5,
+                            style: 'bold'
                         }
-                    },
-                    y: {
-                        title: {
-                            text: 'Wartość pomiaru',
-                            display: true,
-                            font: {
-                                size: 20,
-                                style: 'bold'
-                            }
+                    }
+                },
+                y: {
+                    title: {
+                        text: 'Wartość pomiaru',
+                        display: false,
+                        font: {
+                            size: 5,
+                            style: 'bold'
                         }
                     }
                 }
             }
-        });
-    }
-
-    clickAndDraw(10, 'id', 'asc');
+        }
+    });
+}
